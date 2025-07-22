5fdd2436645bfc335e25b7652d4e4d4ebae98ffa
Ivan Bro <lama.king.2000@mail.ru>
2025-07-22 21:28:14 +0300

    add simple logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/lamaking48/RESTfull.git/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -12,7 +20,39 @@ func main() {
 	cfg := config.MustLoad()
 
 	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("strting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 }
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
+
 func init() {
 	godotenv.Load() // Загружает переменные из .env
 }
+
+// go run cmd/url-shortener/main.go
---
+++ internal/storage/sqlite/sqlite.go
@@ -0,0 +1,19 @@
+package sqlite
+
+// import "database/sql"
+
+// type Storage struct {
+// 	db *sql.DB
+// }
+
+// func New(storagePath string) (*Storage,error) {
+
+// 	const op = "storage.sqlite.New"
+
+// 	db,err := sql.Open("sqlite3", "./url-shortener.db")
+
+// 	if err != nil {
+// 		return nil, fmt.Errorf("%s,%w",op,err)
+// 	}
+
+// }
